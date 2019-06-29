321ccb75d64a3d20d30f6901d6a20c5a42d912e5
QueraltSosaMompel <queraltsosa@hotmail.com>
2019-06-29 23:04:07 +0100

    V(n) was updated

--- V(n)/main.py
+++ V(n)/main.py
@@ -3,7 +3,7 @@ from mem import *
 from tab import *
 
 print "Dynamic programming for the recurrence relationship:"
-print "V(1) = 1 otherwise V(n+1) = 1 + V(n+1-V(V(n)))"
+print "V(1) = V(2) = 1 otherwise V(n) = V (V (n-1)) + V(n – V(n-1))"
 print "Tabulation:"
 start_time = time.clock()
 tab_init()
